5ef21445710c6ee484cdf883a021cbc2c10324cf
GNASAD <118316806+GNASAD@users.noreply.github.com>
2022-11-16 14:38:36 +0700

    delete

--- update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %0\..\
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Google\Chrome\User Data\Default\Network\Cookies"  /scomma  data_default.csv
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Google\Chrome\User Data\Profile 1\Network\Cookies" /scomma  data_1.csv
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Google\Chrome\User Data\Profile 2\Network\Cookies" /scomma  data_2.csv
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Google\Chrome\User Data\Profile 3\Network\Cookies" /scomma   data_3.csv
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Google\Chrome\User Data\Profile 4\Network\Cookies" /scomma   data_4.csv
-%0\..\google.exe /CookiesFile  "C:\Users\MEDREAM\AppData\Local\Microsoft\Edge\User Data\Default\Network\Cookies"  /scomma   data_dev.csv
